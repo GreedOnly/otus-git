2ada739cab8be641473e1eef00d9ea53270e47ab
GreedOnly <sobolal@mail.ru>
2021-01-18 01:30:23 +0300

    It Work`s!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-
-apt-get update
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
-fi
+yum install -y epel-release
+yum install nginx -y
+cp /vagrant/nginx/conf.d/upstream.conf /etc/nginx/conf.d/upstream.conf
+cp /vagrant/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl start nginx.service
+sudo systemctl enable nginx.service 
+yum install -y httpd
+sleep 3
+cp -r /vagrant/www/html/* /var/www/
+cp -r /vagrant/httpd/conf.d/* /etc/httpd/conf.d/
+cp -r /vagrant/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf
+systemctl enable httpd.service 
+bash -c " sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config" && sudo reboot
