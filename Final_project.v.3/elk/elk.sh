23773ce8871897695a7980e8697f155304cb8a6a
Александр Соболев <asobolev@astralinux.ru>
2021-03-27 01:43:08 +0300

    ref:all

--- Final_project.v.3/elk/elk.sh
+++ Final_project.v.3/elk/elk.sh
@@ -7,7 +7,7 @@ systemctl enable elasticsearch.service
 systemctl start elasticsearch.service
 
 apt install curl -y
-cp /vagrant/elasticsearch.yml /etc/elasticsearch/
+cp /vagrant/elk/elasticsearch.yml /etc/elasticsearch/
 systemctl restart elasticsearch.service
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
@@ -15,19 +15,19 @@ apt update && apt install kibana
 systemctl daemon-reload
 systemctl enable kibana.service
 systemctl start kibana.service
-cp /vagrant/kibana.yml /etc/kibana/  
+cp /vagrant/elk/kibana.yml /etc/kibana/  
 systemctl restart kibana.service
 
 apt install logstash
 systemctl enable logstash.service        
-cp input.conf /etc/logstash/conf.d
-cp output.conf /etc/logstash/conf.d
-cp filter.conf /etc/logstash/conf.d
+cp /vagrant/elk/input.conf /etc/logstash/conf.d
+cp /vagrant/elk/output.conf /etc/logstash/conf.d
+cp /vagrant/elk/filter.conf /etc/logstash/conf.d
 systemctl start logstash.service
 
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.11.0-amd64.deb
 dpkg -i filebeat-7.11.0-amd64.deb
-cp /vagrant/filebeat.yml /etc/filebeat/
+cp /vagrant/elk/filebeat.yml /etc/filebeat/
 systemctl start filebeat
 systemctl enable filebeat
 
